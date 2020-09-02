2669246987e0b1f404984b0d1503662175ec0527
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2020-09-01 22:54:52 -0500

    Update router.go

--- router/router.go
+++ router/router.go
@@ -31,9 +31,10 @@ func SDRouter() (*gin.Engine, error) {
 	restRouter.GET("/", handlers.HealthCheckHandler)
 	restRouter.GET("/healthz", handlers.HealthCheckHandler)
 	restRouter.GET("/api/v1/healthz", handlers.HealthCheckHandler)
-	restRouter.POST("/api/v1/hello", handlers.UserRegistrationHandler)
 	// ....................................................................
 	// TODO: add any future routes here
+	restRouter.POST("/api/v1/hello", handlers.UserRegistrationHandler)
+
 	//
 	return restRouter, nil
 }
