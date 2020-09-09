7c7d7c8cf7c74ac78c09a0354f59eabc62ca4754
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2020-09-09 14:21:44 -0500

    emailId

--- contracts/user_contract.go
+++ contracts/user_contract.go
@@ -4,7 +4,7 @@ package contracts
 // We will secure the connection to backend via SSL/TLS certificates over HTTPS
 // So we dont care to send over these details without hashing over the internet
 type UserRegistrationRequest struct {
-	EmailID       string `json:"emailID" valid:"required"`
+	EmailID       string `json:"emailId" valid:"required"`
 	UserRole 	string `json:"userRole" valid:"required"`
 	PhoneNumber string `json:"phoneNumber"`
 }
