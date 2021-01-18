5d40e553c7ace078d608c3728b1329d25144a62c
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2021-01-18 08:58:19 -0600

    ScheduleDemo

--- router/router.go
+++ router/router.go
@@ -62,6 +62,7 @@ func SDRouter() (*gin.Engine, error) {
 	referralGroup := version1.Group("/")
 	{
 		referralGroup.POST("/referral/mail", handlers.ReceiveReferralMail)
+		referralGroup.POST("/referral/scheduledemo", handlers.ScheduleDemo)
 		referralGroup.POST("/referral/sms", handlers.TextRecievedPatient)
 		referralGroup.POST("/referrals", handlers.CreateRefSpecialist)
 		referralGroup.POST("/referrals/:referralId/messages", handlers.AddCommentsToReferral)
