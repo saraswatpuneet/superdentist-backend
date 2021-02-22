6e7c537288918affca7796ef223b81c7e9c17137
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2021-02-22 09:02:10 -0600

    Update ref_update_specialist.go

--- handlers/ref_update_specialist.go
+++ handlers/ref_update_specialist.go
@@ -39,7 +39,7 @@ func AddCommentsToReferral(c *gin.Context) {
 	referralID := c.Param("referralId")
 
 	var referralDetails contracts.ReferralComments
-	userEmail, _, gproject, err := getUserDetails(ctx, c.Request)
+	_, _, gproject, err := getUserDetails(ctx, c.Request)
 	if err != nil {
 		c.AbortWithStatusJSON(
 			http.StatusInternalServerError,
