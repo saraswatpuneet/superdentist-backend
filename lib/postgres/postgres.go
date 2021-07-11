f117e62f89385bc63212276ced9d13ef63f4c4d5
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2021-07-10 22:13:56 -0500

    Update postgres.go

--- lib/postgres/postgres.go
+++ lib/postgres/postgres.go
@@ -18,7 +18,7 @@ func NewPostgresHandler() (*PGSDentist, error) {
 	rootCert := "sslrootcert=" + global.Options.RootCA + " "
 	sslKey := "sslkey=" + global.Options.SSLKey + " "
 	sslCert := "sslcert=" + global.Options.SSLCert + " "
-	connection := fmt.Sprintf("host=%s port=%v user=%s dbname=%s sslmode=require password=%s", global.Options.DBHost, global.Options.DBPort, global.Options.DBUser, global.Options.DBName, global.Options.DBPassword)
+	connection := fmt.Sprintf("host=%s port=%v user=%s dbname=%s sslmode=%s password=%s", global.Options.DBHost, global.Options.DBPort, global.Options.DBUser, global.Options.DBName,global.Options.SSLMode, global.Options.DBPassword)
 	connection += " " + rootCert + sslKey + sslCert
 	db, err := sql.Open("postgres", connection)
 	if err != nil {
